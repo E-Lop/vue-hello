2f253041954b4755ef5652ec7fa069b540d96f19
E-Lop <edmondolopez@gmail.com>
2022-06-23 13:54:40 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    messageByVue: 'Hello World',
+    image:
+      'https://media-assets.wired.it/photos/615c496336911c2d72f97331/master/pass/1415182318_2001_a_space_odyssey_scimmia.jpg',
+  },
+  methods: {},
+});
