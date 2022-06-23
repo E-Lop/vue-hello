24213fd49ec68f756ded3613b35454f947984632
E-Lop <edmondolopez@gmail.com>
2022-06-23 16:21:44 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -10,5 +10,4 @@ var app = new Vue({
     image:
       'https://media-assets.wired.it/photos/615c496336911c2d72f97331/master/pass/1415182318_2001_a_space_odyssey_scimmia.jpg',
   },
-  methods: {},
 });
